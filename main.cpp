1965d2911c37ab80dde81c8f198d3ecb04173b99
marc0lhw <gusdn0008@gmail.com>
2017-09-25 20:47:27 -0700

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,6 @@
 #include <stdio.h>
 #include <netinet/ip.h>
 #include <libnet.h>
-#include <libnet-macros.h>
-#include <libnet-headers.h>
-
 
 void usage() {
   printf("syntax: pcap_test <interface>\n");
@@ -30,6 +27,8 @@ int main(int argc, char* argv[]) {
   while (true) {
     struct pcap_pkthdr* header;
     struct libnet_ethernet_hdr* ETH_header;
+    struct libnet_ipv4_hdr* IP_header;
+    struct libnet_tcp_hdr* TCP_header;
     const u_char* packet;
     int res = pcap_next_ex(handle, &header, &packet);
     if (res == 0) continue;
@@ -38,23 +37,64 @@ int main(int argc, char* argv[]) {
 
 	ETH_header = (libnet_ethernet_hdr*)packet;
 	//printf("ETH dest : %x\n", ETH_header->ether_dhost);
-	printf("ETH dest : ");
-	for(int i = 0; i<6; i++)
-		printf("%02x ", ETH_header->ether_dhost[i]);
+	printf("ETH dst : ");
+	for(int i = 0; i<6; i++) {
+		printf("%02x", ETH_header->ether_dhost[i]);
+		if(i<5) printf(":");
+	}
 	printf("\n");
 	printf("ETH src : ");
-        for(int i = 0; i<6; i++)
-                printf("%02x ", ETH_header->ether_shost[i]);
+        for(int i = 0; i<6; i++) {
+                printf("%02x", ETH_header->ether_shost[i]);
+		if(i<5) printf(":");
+	}
         printf("\n");
 
-	printf("ETH type : %04x \n", ntohs(ETH_header->ether_type));
-
+	printf("ETH type : %04x ", ntohs(ETH_header->ether_type));
 	if(ntohs(ETH_header->ether_type) == 0x0800)
-		printf("It's IP!\n");
+		printf("	-> It's IP!\n");
+	else {
+		printf("\n");
+		continue;
+	}
+	
+	packet += sizeof(struct libnet_ethernet_hdr);
+	IP_header = (libnet_ipv4_hdr*)packet;
+	printf("IP dst : %s\n", inet_ntoa(IP_header->ip_dst));
+	printf("IP src : %s\n", inet_ntoa(IP_header->ip_src));
+	//uint32_t tmp = 0x00FF0000;
+	//tmp = tmp ^ IP_header->ip_dst;
+        //printf("IP dst2 : %x\n", IP_header->ip_dst);        
+	//printf("IP dst3 : %x\n", tmp);
 
-	
-	
-  }
+	printf("IP protocol : %02x ", IP_header->ip_p);	
+	if(IP_header->ip_p == 0x06)
+		printf("	-> It's TCP!\n");
+	else {
+		printf("\n");
+		continue;
+	}
+
+	packet += sizeof(struct libnet_ipv4_hdr);
+	TCP_header = (libnet_tcp_hdr*)packet;
+	printf("TCP dst : %d\n", TCP_header->th_dport);
+	printf("TCP src : %d\n", TCP_header->th_sport);
+
+/*	
+struct libnet_tcp_hdr
+{
+    u_int16_t th_sport;       // source port 
+    u_int16_t th_dport;       // destination port 
+*/	
+
+	// 패킷 길이가 남아있으면 데이터 존재 -> 데이터 출력
+
+	printf("Data : ");
+	for (int i=0; i<16; i++, packet++)
+		printf("%x", *packet); 
+
+	printf("\n");
+}
 
   pcap_close(handle);
   return 0;
